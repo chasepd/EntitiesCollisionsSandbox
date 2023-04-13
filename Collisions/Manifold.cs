7bf4edc7f09ddc8a41f48ca053a524f0c09bba4f
Chase Davies <chase.davies@sproutsocial.com>
2023-04-13 14:05:17 -0600

    Switch to MIT License

---
+++ Collisions/Manifold.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace EntitiesCollisionsSandbox.Collisions
+{
+    public struct Manifold
+    {
+        public float Penetration;
+        public Vector2 Normal;
+        public Vector2 Overlap;
+    }
+}
